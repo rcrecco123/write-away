ccd78ebb6a2593107e24513fa410fbcf2ed9580e
Ronald Crecco <ronaldcrecco@Ronalds-MacBook-Pro.local>
2020-05-20 14:50:27 -0400

    Updates

---
+++ precache-manifest.fed261c5ab11f29d43f26a7ec2f9d9a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcfe1299ee910dd7674845b438dd262",
+    "url": "/write-away/index.html"
+  },
+  {
+    "revision": "7002a436cde1e1bfea58",
+    "url": "/write-away/static/css/main.443b437c.chunk.css"
+  },
+  {
+    "revision": "ad808f41fa33d1223022",
+    "url": "/write-away/static/js/2.84590e10.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/write-away/static/js/2.84590e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7002a436cde1e1bfea58",
+    "url": "/write-away/static/js/main.e92e10c1.chunk.js"
+  },
+  {
+    "revision": "4c1721803df691afad9f",
+    "url": "/write-away/static/js/runtime-main.6f233dab.js"
+  },
+  {
+    "revision": "e52bb476d983c46c7e9f691a77e0bb49",
+    "url": "/write-away/static/media/MASTER.e52bb476.png"
+  },
+  {
+    "revision": "f77018198af719d0d4e89ae69fd97e48",
+    "url": "/write-away/static/media/family3.f7701819.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/write-away/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ca3835ad9d7f9c9cf923decdd0258ca",
+    "url": "/write-away/static/media/output-onlinepngtools.4ca3835a.png"
+  },
+  {
+    "revision": "a97ab53876b22ce684b539815dd0af89",
+    "url": "/write-away/static/media/whiteboard4.a97ab538.png"
+  }
+]);
