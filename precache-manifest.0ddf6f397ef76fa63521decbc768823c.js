d5c215c4c40af97dccbc9ce96e84fb0de4ff338a
Ronald Crecco <ronaldcrecco@Ronalds-MacBook-Pro.local>
2020-05-21 16:38:26 -0400

    Updates

---
+++ precache-manifest.0ddf6f397ef76fa63521decbc768823c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28895ca3ff23cf3fb707115a9fbb4eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cc8df4b204574b70e4f",
+    "url": "/static/css/main.443b437c.chunk.css"
+  },
+  {
+    "revision": "34178beb5226b5a60157",
+    "url": "/static/js/2.8de59eef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8de59eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc8df4b204574b70e4f",
+    "url": "/static/js/main.ddf55d52.chunk.js"
+  },
+  {
+    "revision": "0a9c021c15e7bc64c69f",
+    "url": "/static/js/runtime-main.d2940411.js"
+  },
+  {
+    "revision": "e52bb476d983c46c7e9f691a77e0bb49",
+    "url": "/static/media/MASTER.e52bb476.png"
+  },
+  {
+    "revision": "f77018198af719d0d4e89ae69fd97e48",
+    "url": "/static/media/family3.f7701819.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ca3835ad9d7f9c9cf923decdd0258ca",
+    "url": "/static/media/output-onlinepngtools.4ca3835a.png"
+  },
+  {
+    "revision": "a97ab53876b22ce684b539815dd0af89",
+    "url": "/static/media/whiteboard4.a97ab538.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/write-away/precache-manifest.a979ed37ab5025db26a55928dc1e7a4a.js"
+  "/precache-manifest.0ddf6f397ef76fa63521decbc768823c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/write-away/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
